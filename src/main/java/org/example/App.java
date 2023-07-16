8e2d43f7eb1f336b56233d38e6712ad66c545bb0
mratneshwar <m.ratneshwar@gmail.com>
2023-07-16 21:43:46 +0530

    third commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,9 +7,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        for(int i=0;i<=2;i++)
+            System.out.println("for loop");
+
+        System.out.println( "Rahul" );
+        System.out.println( "Amit Malvi" );
 
     }
 }
