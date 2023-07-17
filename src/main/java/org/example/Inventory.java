c37bde5004e4fc003fab4cf904219f8be07cd21e
mratneshwar <m.ratneshwar@gmail.com>
2023-07-17 17:58:22 +0530

    Dev Branch2 Commit1

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -1,10 +1,5 @@
 package org.example;
 
 public class Inventory {
-    Inventory(){
-        System.out.println("inventory");
-        System.out.println("Line 1 Added in 1 commit");
-        System.out.println("Line 2 Added in 2 commit");
-        System.out.println("Line 3 Added in 3 commit");
-    }
+
 }
--- src/main/java/org/example/ItemCategoryMaster.java
+++ src/main/java/org/example/ItemCategoryMaster.java
@@ -1,7 +1,5 @@
 package org.example;
 
 public class ItemCategoryMaster {
-    ItemCategoryMaster(){
-        System.out.println("Ram");
-    }
+
 }
