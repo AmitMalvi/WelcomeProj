912b0a41abca9a933f0412b73a38a97c2c82c655
AmitMalvi <88929663+AmitMalvi@users.noreply.github.com>
2023-07-17 14:02:08 +0530

    Update ItemMaster.java
    ItemMaster commit on server by Amit Malvi

--- src/main/java/org/example/ItemMaster.java
+++ src/main/java/org/example/ItemMaster.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class ItemMaster {
     ItemMaster(){
-        System.out.println("ItemMaster.java default constructor");
+        System.out.println("ItemMaster");
     }
     ItemMaster(String str){
         System.out.println("ItemMaster.java parameter  ized constructor ::"+str);
