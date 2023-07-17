4feb924bbd55c1929b0bbbc9e252a1752eacfa13
mratneshwar <m.ratneshwar@gmail.com>
2023-07-17 16:00:01 +0530

    Commit 2

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -4,5 +4,6 @@ public class Inventory {
     Inventory(){
         System.out.println("inventory");
         System.out.println("Line 1 Added in 1 commit");
+        System.out.println("Line 2 Added in 2 commit");
     }
 }
