37f08e712df305b26f68e5b6e30b93d0373939e8
mratneshwar <m.ratneshwar@gmail.com>
2023-07-17 17:59:54 +0530

    Dev Branch3 Commit1

--- src/main/java/org/example/Distributor.java
+++ src/main/java/org/example/Distributor.java
@@ -1,22 +1,6 @@
 package org.example;
 
-public class Distributor {
+public class Distributor
+{
 
-    Distributor(){
-        System.out.println("printing from Distributor.class");
-    }
-
-    Distributor(String s)
-    {
-        System.out.println("Distributor.class paramenterizd construto :: " +s);
-    }
-
-    Distributor(Integer i)
-    {
-        System.out.println("Distributor.class paramenterizd construto :: " +i);
-    }
-    Distributor(Integer i,String str)
-    {
-        System.out.println("Distributor.class paramenterizd construto :: " +i+" "+ str);
-    }
 }
